4db9fa9788c508f5ecdb2c822cd9770761860023
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-04-26 23:19:55 +0300

    new

--- src/app/components/HeadLineCards.tsx
+++ src/app/components/HeadLineCards.tsx
@@ -1,38 +1,45 @@
-const HeadLineCards = () =>{
-    return(
-        <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
-            <div className='relative rounded-xl'>
-                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                    <p className='font-bold text-2xl px-2 pt-4 '>Sun's Out, BOGO's Out</p>
-                    <p className='px-2'>Throught out</p>
-                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
-                </div>
-                <img className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
-                 src="./eggs.avif" alt="" />
-            </div>
-            <div className='relative rounded-xl'>
-                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                    <p className='font-bold text-2xl px-2 pt-4 '>Sun's Out, New Restaurants</p>
-                    <p className='px-2'>Add Daily</p>
-                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
-                </div>
-                <img className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
-                 src="./photo-1.avif" alt="" />
-            </div>
-            <div className='relative rounded-xl'>
-                <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-                    <p className='font-bold text-2xl px-2 pt-4 '>We Deliver Desserts Too</p>
-                    <p className='px-2'>Tasty Treats</p>
-                    <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
-                </div>
-                <img className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
-                 src="./photo-12.avif" alt="" />
-            </div>
-            
-            
+const HeadLineCards = () => {
+    return (
+      <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
+        <div className='relative rounded-xl'>
+          <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-4 '>Sun&apos;s Out, BOGO&apos;s Out</p>
+            <p className='px-2'>Throughout</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          </div>
+          <img
+            className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
+            src='./eggs.avif'
+            alt=''
+          />
         </div>
-    )
-     
-}
-
-export default HeadLineCards;
+        <div className='relative rounded-xl'>
+          <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-4 '>Sun&apos;s Out, New Restaurants</p>
+            <p className='px-2'>Added Daily</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          </div>
+          <img
+            className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
+            src='./photo-1.avif'
+            alt=''
+          />
+        </div>
+        <div className='relative rounded-xl'>
+          <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
+            <p className='font-bold text-2xl px-2 pt-4 '>We Deliver Desserts Too</p>
+            <p className='px-2'>Tasty Treats</p>
+            <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
+          </div>
+          <img
+            className='max-h-[160px] md:max-[200px] w-full object-cover rounded-xl'
+            src='./photo-12.avif'
+            alt=''
+          />
+        </div>
+      </div>
+    );
+  };
+  
+  export default HeadLineCards;
+  
