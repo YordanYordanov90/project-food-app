ac988760ce74cab25b4f4bd9f377af31b7bab043
YordanYordanov90 <157980191+YordanYordanov90@users.noreply.github.com>
2024-04-26 23:24:34 +0300

    last

--- src/app/components/HeadLineCards.tsx
+++ src/app/components/HeadLineCards.tsx
@@ -3,7 +3,7 @@ const HeadLineCards = () => {
       <div className='max-w-[1640px] mx-auto p-4 py-12 grid md:grid-cols-3 gap-6'>
         <div className='relative rounded-xl'>
           <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-            <p className='font-bold text-2xl px-2 pt-4 '>Sun&apos;s Out, BOGO&apos;s Out</p>
+            <p className='font-bold text-2xl px-2 pt-4 '>Sun and Out</p>
             <p className='px-2'>Throughout</p>
             <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
           </div>
@@ -15,7 +15,7 @@ const HeadLineCards = () => {
         </div>
         <div className='relative rounded-xl'>
           <div className='absolute w-full h-full bg-black/50 rounded-xl text-white'>
-            <p className='font-bold text-2xl px-2 pt-4 '>Sun&apos;s Out, New Restaurants</p>
+            <p className='font-bold text-2xl px-2 pt-4 '>Suns Out, New Restaurants</p>
             <p className='px-2'>Added Daily</p>
             <button className='border-white bg-white text-black mx-2 absolute bottom-4'>Order Now</button>
           </div>
